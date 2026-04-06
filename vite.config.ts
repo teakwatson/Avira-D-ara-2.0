0792cec2189b299d648e576a36bef1dc9811823a
teakwatson <teakwatson2008@gmail.com>
2026-04-06 23:08:46 +0200

    Add vite.config.ts configuration file

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    strictPort: false
+  }
+});
